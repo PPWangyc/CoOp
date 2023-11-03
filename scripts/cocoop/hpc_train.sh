0e21c7ca122ccc53cc13584c5aedc51fc3ead96e
PPWangyc <ppwang@stanford.edu>
2023-11-03 19:31:02 -0400

    add

--- scripts/cocoop/hpc_train.sh
+++ scripts/cocoop/hpc_train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DATASET=caltech101
+DATASET=ucf101
 for seed in {1..3}; do
     echo "seed ${seed}"
     sbatch --gres=gpu:rtx8000:1 -N 1 -n 1 --ntasks-per-node=1 --cpus-per-task=8 \
-        --job-name=cocoop-test-class --output=cocoop-test-class.out \
+        --job-name=cocoop-test-class --output=cocoop-train-class.out \
         --mem=64G --time=36:00:00 base2new_train.sh ${DATASET} ${seed} 
 done
